07d1e156ae22e515f0795e89f876c6836dc92efc
Nils <nilshg@hotmail.com>
2014-11-06 14:26:21 +0000

    Create HL_0_JuliaCode.jl

---
+++ HL/HL_0_JuliaCode.jl
@@ -0,0 +1,19 @@
+using DataFrames
+using PyPlot
+using Distributions
+using Optim
+using QuantEcon
+using Grid
+
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Plots.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Optimizations.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Interpolations.jl")
+
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Parameters.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Income.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Beliefs.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Grid.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Retirement.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Transition.jl")
+include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Bellman.jl")
+#include("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Julia\\With Habits\\Simulate.jl")
