7754a503b70b0ae4133c7890fcde3d1b03aeabc0
Nils <nilshg@hotmail.com>
2014-11-25 17:44:26 +0000

    Update Optimizations.jl

--- Optimizations.jl
+++ Optimizations.jl
@@ -3,7 +3,7 @@
 ################################################################################
 
 # Contains:
-# BellOpt(w, h, a, b, z, wmin, V_int, Yln, u, R, lambda, dbeta, t):
+# BellOpt(w, h, a, b, z, wmin, V_int, Yln, u, R, lambda, \delta, t):
 #     Solve value function optimization for state (x,h,a,b,z) using
 #     Gauss-Legendre Quadrature
 #
@@ -19,7 +19,7 @@
 #
 # BellOpt_TRANS(w, y, yfixed, wmin, V_interpol, u, R, dbeta, t)
 #     Solve transition period optimizatoin problem without habits
-# 
+#
 # BellOpt_R(w, y, wmin, V_int, u, R, dbeta, t)
 #     Solve retirement optimization problem without habits
 
@@ -27,168 +27,160 @@
 
 function BellOpt{T<:Float64}(w::T, h::T, y::T, a::T, b::T, z::T, wmin::T,
                              V_int::CoordInterpGrid, Yln::LogNormal, u::Function,
-                             R::T, lamdba::T, dbeta::T, t::Real)
+                             R::T, λ::T, δ::T, t::Real)
 
    x = w + y
 
-  function EVprime_leg(wp::Real, x = x, h = h, a=a, b=b, z=z, t=t,
-                       Yln = Yln, V_int = V_int)
-    y_l = y - 3*Yln.sdlog
-    y_h = y + 3*Yln.sdlog
+  function EVprime_leg(w′::Real, x=x, h=h, a=a, b=b, z=z, t=t, Yln=Yln, V_int=V_int)
 
-    hp = (1-lambda)*h + lambda*(x - wp/R)
+    y_l = Yln.meanlog - 3*Yln.sdlog
+    y_h = Yln.meanlog + 3*Yln.sdlog
 
-    function EVp(y::Array{Float64,1}, wp = wp, hp = hp,
+    h′ = (1-λ)*h + λ*(x - w′/R)
+
+    function EVp(y::Array{Float64,1}, w′=w′, h′=h′,
                  V_int = V_int, Yln = Yln, a=a, b=b, z=z)
       result = zeros(size(y, 1), 1)
       for i = 1:size(y, 1)
-        result[i, :] = V_int[wp + y[i], hp, a, b, z]*pdf(Yln, y[i])
+        result[i, :] = V_int[w′ + y[i], h′, a, b, z]*pdf(Yln, y[i])
       end
       return result
     end
 
-    function EVp(y::Float64, wp = wp, hp=hp,
+    function EVp(y::Float64, w′=w′, h′=h′,
                  V_int = V_int, Yln =Yln, a = a, b = b, z = z)
-      return V_int[wp + y, hp, a, b, z]*pdf(Yln, y)
+      return V_int[w′ + y, h′, a, b, z]*pdf(Yln, y)
     end
 
-    quadrect(EVp, 9, y_l, y_h)
+    quadrect(EVp, 9, exp(y_l), exp(y_h))
   end
 
-  Blmn(wp::Float64) = -( u(x - wp/R, (1 - lambda)*h + lambda*(x - wp/R))
-                         + dbeta * EVprime_leg(wp) )
+  Blmn(w′::Float64) = -(u(x-w′/R, (1-λ)*h + λ*(x-w′/R)) + δ*EVprime_leg(w′))
 
-  if x - 0.01 <= wmin
-    wprime = wmin
-    Vopt = Blmn(wmin)
-  else
-    Optimum = optimize(Blmn, wmin, x - 0.01)
-    wprime = Optimum.minimum
-    Vopt = -(Optimum.f_minimum)
-  end
-  return wprime, Vopt
+  Optimum = optimize(Blmn, wmin, x - 0.01)
+  w′ = Optimum.minimum
+  Vopt = -(Optimum.f_minimum)
+
+  return w′  , Vopt
 end
 
 ################################################################################
 
 function BellOpt_TRANS{T<:Float64}(w::T, h::T, y::T, yfixed::T, wmin::T,
                                    V_int::CoordInterpGrid, u::Function,
-                                   R::T, dbeta::T, lambda::T, t::Int64)
+                                   R::T, δ::T, λ::T, t::Int64)
 
   x = w + y
 
-  Blmn(wp) = -( u(x - wp/R, h)
-                + dbeta * V_int[wp, (1-lambda)*h + lambda*(x-wp/R), yfixed])
+  Blmn(w′::Float64) = -(u(x - w′/R, h) + δ*V_int[w′, (1-λ)*h + λ*(x-w′/R), yfixed])
 
-  if x - 0.01 <= wmin
-    wprime = wmin
-    Vopt = -(Blmn(wmin))
-    if h == 1
-      @printf "\t At [w,y,q] = [%d,%d,%d] consumer is in a corner\n" w y q
-    end
-  else
-    Optimum = optimize(Blmn, wmin, x - 0.01)
-    wprime = Optimum.minimum
-    Vopt = -(Optimum.f_minimum)
-  end
-  return wprime, Vopt
+  Optimum = optimize(Blmn, wmin, x - 0.01)
+  w′ = Optimum.minimum
+  Vopt = -(Optimum.f_minimum)
+
+  return w′, Vopt
 end
 
 ################################################################################
 
 function BellOpt_R{T<:Float64}(w::T, h::T, y::T, wmin::T,
                    V_int::CoordInterpGrid, u::Function,
-                   R::T, dbeta::T, lambda::T, t::Int64)
+                   R::T, δ::T, λ::T, t::Int64)
 
   x = w + y
 
-  Blmn(wp) = -( u(x - wp/R, h)
-                + dbeta * V_int[wp, (1-lambda)*h + lambda*(x-wp/R), y])
+  Blmn(w′) = -(u(x - w′/R, h) + δ*V_int[w′, (1-λ)*h + λ*(x-w′/R), y])
 
   Optimum = optimize(Blmn, wmin, x - 0.01)
-
-  wprime = Optimum.minimum
+  w′ = Optimum.minimum
   Vopt = -(Optimum.f_minimum)
 
-  return wprime, Vopt
+  return w′, Vopt
 end
 
 #######################################################################################
 
-function BellOpt{T<:Float64}(wt::T, yt::T, at::T, bt::T, zt::T, wmin::T,
+function BellOpt{T<:Float64}(wt::T, yt::T, a::T, b::T, z::T, wmin::T,
                              V_int::CoordInterpGrid, Yln::LogNormal, u::Function,
-                             R::T, dbeta::T, t::Int64)
+                             R::T, δ::T, t::Int64)
 
-   xt = wt + yt
+  x = wt + yt
 
-  function EVprime_leg(wp::Float64, x = xt, a=at, b=bt, z=zt, t=t,
-                       Yln = Yln, V_int = V_int)
-    y_l = yt - 3*Yln.sdlog
-    y_h = yt + 3*Yln.sdlog
+  function EVprime_leg(w′::Float64, a=a, b=b, z=z, t=t, Yln=Yln, V_int=V_int)
+    y_l = Yln.meanlog - 3*Yln.sdlog
+    y_h = Yln.meanlog + 3*Yln.sdlog
 
-    function EVp(y::Array{Float64,1}, wp = wp, V_int = V_int, Yln =Yln, a = a, b = b, z = z)
+    function EVp(y::Array{Float64,1}, w′=w′, V_int=V_int, Yln=Yln, a=a, b=b, z=z)
       result = zeros(size(y, 1), 1)
       for i = 1:size(y, 1)
-        result[i, :] = V_int[wp + y[i], a, b, z]*pdf(Yln, y[i])
+        result[i, :] = V_int[w′ + y[i], a, b, z]*pdf(Yln, y[i])
       end
       return result
     end
 
-    function EVp(y::Float64, wp = wp, V_int = V_int, Yln =Yln, a = a, b = b, z = z)
-      return V_int[wp + y, a, b, z]*pdf(Yln, y)
+    function EVp(y::Float64, w′=w′, V_int=V_int, Yln=Yln, a=a, b=b, z=z)
+      return V_int[w′ + y, a, b, z]*pdf(Yln, y)
     end
 
-    quadrect(EVp, 9, y_l, y_h)
+    quadrect(EVp, 9, exp(y_l), exp(y_h))
   end
 
-  Blmn(wp) = -( u(xt - wp/R) + dbeta * EVprime_leg(wp) )
+  Blmn(w′) = -( u(x - w′/R) + δ * EVprime_leg(w′) )
 
-  if xt - 0.01 <= wmin
-    wprime = wmin
-    Vopt = Blmn(wmin)
-  else
-    Optimum = optimize(Blmn, wmin, xt - 0.01)
-    wprime = Optimum.minimum
-    Vopt = -(Optimum.f_minimum)
-  end
-  return wprime, Vopt
+  Optimum = optimize(Blmn, wmin, t - 0.01)
+  w′ = Optimum.minimum
+  Vopt = -(Optimum.f_minimum)
+
+  return w′, Vopt
 end
 
 ################################################################################
 
 function BellOpt_TRANS{T<:Float64}(w::T, y::T, yfixed::T, wmin::T,
-                   V_interpol::CoordInterpGrid, u::Function,
-                   R::T, dbeta::T, t::Int64)
+                   V_int::CoordInterpGrid, u::Function,
+                   R::T, δ::T, t::Int64)
 
   x = w + y
 
-  Blmn(wp::Float64) = -( u(x - wp/R) + dbeta * V_interpol[wp, yfixed])
+  Blmn(w′) = -( u(x - w′/R) + δ*V_int[w′, yfixed])
 
-  if x - 0.01 <= wmin
-    wprime = wmin
-    Vopt = -(Blmn(wmin))
-  else
-    Optimum = optimize(Blmn, wmin, x - 0.01)
-    wprime = Optimum.minimum
-    Vopt = -(Optimum.f_minimum)
-  end
-  return wprime, Vopt
+  Optimum = optimize(Blmn, wmin, x - 0.01)
+  w′ = Optimum.minimum
+  Vopt = -(Optimum.f_minimum)
+
+  return w′, Vopt
 end
 
 ################################################################################
 
 function BellOpt_R{T<:Float64}(w::T, y::T, wmin::T,
                    V_int::CoordInterpGrid, u::Function,
-                   R::T, dbeta::T, t::Int64)
+                   R::T, δ::T, t::Int64)
 
   x = w + y
 
-  Blmn(wp) = -( u(x - wp/R) + dbeta * V_int[wp, y])
+  Blmn(w′) = -(u(x-w′/R) + δ*V_int[w′, y])
 
   Optimum = optimize(Blmn, wmin, x - 0.01)
-
-  wprime = Optimum.minimum
+  w′ = Optimum.minimum
   Vopt = -(Optimum.f_minimum)
 
-  return wprime, Vopt
+  return w′, Vopt
+end
+
+################################################################################
+
+function BellOpt_R{T<:Float64}(w::T, y::T, wmin::T,
+                   V_int::Spline2D, u::Function,
+                   R::T, δ::T, t::Int64)
+
+  x = w + y
+
+  Blmn(w′) = -(u(x-w′/R) + δ*evaluate(V_int, w′, y))
+
+  Optimum = optimize(Blmn, wmin, x - 0.01)
+  w′ = Optimum.minimum
+  Vopt = -(Optimum.f_minimum)
+
+  return w′, Vopt
 end
