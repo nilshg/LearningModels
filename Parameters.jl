8031ee2523c64a55a7cc4eae606826528c4f3757
Nils <nilshg@hotmail.com>
2015-01-16 10:40:40 +0000

    Conform to Julia style
    Fix usage of capital letters for variable names

--- Parameters.jl
+++ Parameters.jl
@@ -2,11 +2,9 @@
 ################################# PARAMETERS #######################################
 ####################################################################################
 
-tol = 1e-9             # Tolerance Level for Iterations
-TW = 40                # Number of working life periods
-TR = 30                # Number of retirement periods
-r = 0.015              # Real interest rate
-R = 1 + r              # Handy for notation
+tW = 40                # Number of working life periods
+tR = 30                # Number of retirement periods
+r = 1/0.96             # Real Return
 agents = 100           # Number of agents sharing an alpha/beta combination
 bs = 1000              # Different alpha/beta combinations in the population;
 
@@ -16,43 +14,47 @@ bs = 1000              # Different alpha/beta combinations in the population;
 λ = 0.8                # Persistence of habits (lambda=0 --> no persistence)
 σ = 2.0                # Coefficient of relative risk aversion
 
-# Parameters of income distribution
+# Parameters of income process (HIP)
 μₐ = 2.0               # Mean of the income intercept
-μᵦ = 0.005             # Mean of income growth rates
-var_a = 0.005          # Cross-sectional variance of alpha
-var_b = 0.00037        # Cross-sectional variance of beta
-var_eta = 0.029        # Variance of persistent shock
-var_eps = 0.047        # Variance of transitory shock
+μᵦ = 0.009              # Mean of income growth rates
+var_α = 0.005          # Cross-sectional variance of alpha
+var_β = 0.00037        # Cross-sectional variance of beta
+var_η = 0.029          # Variance of persistent shock
+var_ɛ = 0.047          # Variance of transitory shock
 ρ = 0.82               # Persistence of AR(1) component of income
-BR = 30                # Period of structural break (has to be <T)
+br = 30                # Period of structural break (has to be <T)
+
+# Parameters of income process (RIP)
+var_η_RIP = 0.015      # σ²(η)
+var_ɛ_RIP = 0.061      # σ²(ɛ)
+ρ_RIP = 0.988          # AR(1) persistence
 
 # Paremeters for belief calculation
-FPU = 0.65             # Proportion of variance of beta that is known
-init_beta = 0.04       # Initial belief about mean of beta
+fpu = 0.65             # Proportion of variance of beta that is known
+init_β = 0.029         # Initial belief about mean of beta
 init_z = 0.0           # Initial belief about mean of z
-init_varbeta = 0.01    # Initial belief about variance of beta
-init_varz = 0.01       # Initial belief about ariance of z
+init_var_β = 0.01      # Initial belief about variance of beta
+init_var_z = 0.01      # Initial belief about ariance of z
 
 # Parameters for grid construction
-Sgridpoints = 15       # Partitions for belief cube
-wgridpoints = 10       # Wealth grid points (working life)
-wgridpoints_R = 60     # Wealth grid points (retirement)v
-hgridpoints = 7        # Habit grid points (working life)
-hgridpoints_R = 40     # Habit grid points (retirement)
-ygridpoints = 6        # Income gridpoints (working life)
-ygridpoints_R = 50     # Pension gridpoints
+wpoints = 40           # Wealth grid points (working life)
+wpoints_R = 130        # Wealth grid points (retirement)
+hpoints = 7            # Habit grid points (working life)
+hpoints_R = 29         # Habit grid points (retirement)
+ypoints_R = 120        # Pension points
 wmaxR = 1000.0         # Maximum retirement wealth
-agridpoints = 4        # Grid points for beliefs about α
-bgridpoints = 8        # Grid points for beliefs about β
-zgridpoints= 7         # Grid points for beliefs about z
+apoints = 4            # Grid points for beliefs about α
+bpoints = 8           # Grid points for beliefs about β
+zpoints = 7            # Grid points for beliefs about z
+zpoints_RIP = 32       # Grid points for RIP persistent shock
+epspoints = 2          # Grid points for RIP transitory shock
 power = 1.0            # Grid Curvature
-retirement_grid_irregular = false
 
 # Utility function
-function u{T<:Float64}(c::T, h::T, γ=γ, σ=σ)
+function u(c::Float64, h::Float64, γ=γ, σ=σ)
     ((c/(h^γ))^(1-σ))/(1-σ)
 end
 
-function u{T<:Float64}(c::T, σ=σ)
+function u(c::Float64, σ=σ)
      (c^(1-σ))/(1-σ)
 end
