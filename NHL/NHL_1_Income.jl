384e297c4bf696d7d0d96d9d4a87b5e9cad0293a
Nils <nilshg@hotmail.com>
2015-01-16 10:45:12 +0000

    Split up income function
    Rather than having a true/false branch, we now have different functions to create the income distribution, multiple dispatch allows us to change the construction method simply by changing the arguments in the function call.

--- NHL/NHL_1_Income.jl
+++ NHL/NHL_1_Income.jl
@@ -1,73 +1,167 @@
 ################################################################################
 ########################### INCOME DISTRIBUTION ################################
 ################################################################################
-@printf "1. Draw an Income Distribution\n"
+#
+# Contains:
+#
+# (yit, alpha, beta, ymedian) = incomeDistribution(agents, bs, path):
+#   - Get yit from Guvenen's data
+#
+# (yit, alpha, beta, ymedian) = incomeDistribution(agents, μₐ, μᵦ, var_a, var_b,
+#                                                  var_eps, ρ, var_eta, br, tW)
+#   - Draw a new income distribution yit with break parameters br
+#
+#################################################################################
 
-guvenen_distribution = true
+using DataFrames
 
-if guvenen_distribution # Use distribution from Guvenen's paper
-    @printf "\tWe are working with Guvenen's data\n"
-    Yit = readcsv("C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code/LaborReal.csv")
-    alfabeta = readcsv("C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code/alfabeta.csv")
-    alpha = alfabeta[:, 1]
-    alpha = reshape(repmat(alpha, 1, agents)', agents*bs, 1)
-    beta = alfabeta[:, 2]
-    beta = reshape(repmat(beta, 1, agents)', agents*bs, 1)
-else # Draw a new distribution
-    # Draw some alphas and betas
-    alpha = zeros(bs)
-    beta1 = zeros(bs)
-    for i = 1:bs
-        alpha[i] = mu_a + sqrt(var_a)*randn()
-        beta1[i] = mu_b + sqrt(var_b)*randn()
+#################################################################################
+
+function incomeDistribution(agents::Int64, bs::Int64, ypath::String,
+                            abpath::String)
+
+  @printf "1. Draw an Income Distribution\n"
+  @printf "\tWe are working with Guvenen's data\n"
+  yit = readdlm(ypath)
+  alfabeta = readdlm(abpath)
+  α = alfabeta[:, 1]
+  α = reshape(repmat(α, 1, agents)', agents*bs, 1)
+  β = alfabeta[:, 2]
+  β = reshape(repmat(β, 1, agents)', agents*bs, 1)
+
+  # Calculate median income in last period for calculation of retirement benefits
+  ymedian = median(yit[:, end])
+  @printf "\tMedian income in period 40 is %.2f\n" ymedian
+
+  pension = Array(Float64, agents*bs)
+  for i = 1:agents*bs # Directly copied out of Guvenen's code
+    ytemp = yit[i, end]
+    if (ytemp<0.3*ymedian)
+      yfixed=0.9*ytemp
+    elseif (ytemp<=2.0*ymedian)
+      yfixed=0.27*ymedian+0.32*(ytemp-0.3*ymedian)
+    elseif (ytemp<4.1*ymedian)
+      yfixed = 0.81*ymedian+0.15*(ytemp-2.0*ymedian)
+    else
+      yfixed = 1.1*ymedian
     end
+    pension[i] = 0.715*yfixed
+  end
 
-    # Sort
-    sort!(beta1);
+  return yit, α, β, ymedian, pension
+end
 
-    beta2 = zeros(length(beta1))
-    for i in [1:length(beta2)]
-        if i < length(beta2)/2
-            beta2[i] = beta1[i] + sqrt(var_b)*randn()
+#################################################################################
+
+function incomeDistribution(agents::Int64, bs::Int64, μₐ::Float64, μᵦ::Float64,
+                            var_a::Float64, var_b::Float64, var_ɛ::Float64,
+                            ρ::Float64, var_η::Float64, br::Int64, tW::Int64)
+
+  # Draw some alphas and betas
+  α = Array(Float64, bs)
+  β_1 = Array(Float64, bs)
+  for i = 1:bs
+      α[i] = μₐ + sqrt(var_a)*randn()
+      β_1[i] = μᵦ + sqrt(var_b)*randn()
+  end
+
+  # Sort
+  sort!(β_1);
+
+  β_2 = Array(Float64, length(β_1))
+  for i in [1:length(β_2)]
+      if i < length(β_2)/2
+          β_2[i] = β_1[i] + sqrt(var_b)*randn()
+      else
+          β_2[i] = β_1[i] + sqrt(var_b)*randn()+0.03
+      end
+  end
+
+  # Create one beta matrix that holds each agents beta for each year
+  β = zeros(agents*bs, tW)
+
+  for t = 1:tW
+      for i = 1:bs
+          if t < br
+              β[(i-1)*agents+1:agents*i,t] = β_1[i]
+          else
+              β[(i-1)*agents+1:agents*i,t] = β_2[i]
+          end
+      end
+  end
+
+  # Reshape α into 100,000x1 vector with each of the 1,000 unique values repeated 100 times
+  α = reshape(repmat(α,1,100)', agents*bs, 1)
+
+  # Draw the income distribution:
+  yit = zeros(bs*agents, tW)
+  z = zeros(bs*agents, tW)
+
+  for t = 1:tW
+      for i = 1:bs*agents
+          yit[i, t] = exp(α[i] + β[i, t]*t + z[t] + sqrt(var_ɛ)*randn())
+          if t < tW
+              z[i, t+1] = ρ*z[i, t] + sqrt(var_η)*randn()
+          end
+      end
+  end
+  @printf "\tβ is between %.2f and %.2f, β_2 is between %.2f and %.2f\n" minimum(β_1)  maximum(β_1) minimum(β_2) maximum(β_2)
+
+  # Calculate median income in last period for calculation of retirement benefits
+  ymedian = median(yit[:, end])
+  @printf "\tMedian income in period 40 is %.2f\n" ymedian
+
+  pension = Array(Float64, agents*bs)
+  for i = 1:agents*bs # Directly copied out of Guvenen's code
+    ytemp = yit[i, end]
+    if (ytemp<0.3*ymedian)
+      yfixed=0.9*ytemp
+    elseif (ytemp<=2.0*ymedian)
+      yfixed=0.27*ymedian+0.32*(ytemp-0.3*ymedian)
+    elseif (ytemp<4.1*ymedian)
+      yfixed = 0.81*ymedian+0.15*(ytemp-2.0*ymedian)
+    else
+      yfixed = 1.1*ymedian
+    end
+    pension[i] = 0.715*yfixed
+  end
+
+  return yit, α, β, ymedian, pension
+end
+
+################################################################################
+
+function incomeDistribution(α::Float64, β::Float64, var_η_RIP::Float64,
+                            var_ɛ_RIP::Float64, zgridpoints_RIP::Int64,
+                            epsgridpoints::Int64, tW::Int64)
+
+  @printf "1. Drawing an RIP-consistent income distribution\n"
+  @printf "\tσ²(η) = %.3f,  σ²(ɛ) =  %.3f\n" var_η_RIP var_ɛ_RIP
+
+  zdisc = [-(1/2)*(zgridpoints_RIP-1)*sqrt(var_η_RIP)+(i-1)*sqrt(var_η_RIP)
+               for i = 1:zgridpoints_RIP]
+
+  epsdisc = [-(1/2)*(epsgridpoints-1)*sqrt(var_ɛ_RIP)+(i-1)*sqrt(var_ɛ_RIP)
+               for i = 1:epsgridpoints]
+
+  transitionz = readdlm("C:\\Users\\tew207\\Dropbox\\QMUL\\PhD\\Code\\Guvenen FORTRAN Code\\RIP_BASELINE_ALL\\Probz.dat")
+
+  yit = Array(Float64, (zgridpoints_RIP, epsgridpoints, tW))
+  for t = 1:tW
+    for z = 1:zgridpoints_RIP
+      for ɛ = 1:epsgridpoints
+        if t < 2
+          yit[:, ɛ, t] = exp(α + β*t + 0.8*zdisc[z] + 0.6*epsdisc[ɛ])
+        elseif t < 3
+          yit[:, ɛ, t] = exp(α + β*t + zdisc[z] + epsdisc[ɛ])
+        elseif t < 5
+          yit[:, ɛ, t] = exp(α + β*t + zdisc[z] + epsdisc[ɛ])
         else
-            beta2[i] = beta1[i] + sqrt(var_b)*randn()+0.03
+          yit[:, ɛ, t] = exp(α + β*t + zdisc[z] + epsdisc[ɛ])
         end
+      end
     end
-    # Set break point at which growth rates switches from beta to beta2
-    BR = 30;
+  end
 
-    # Create one beta matrix that holds each agents beta for each year
-    beta = zeros(agents*bs, T)
-
-    for t = 1:T
-        for i = 1:bs
-            if t < BR
-                beta[(i-1)*agents+1:agents*i,t] = beta1[i]
-            else
-                beta[(i-1)*agents+1:agents*i,t] = beta2[i]
-            end
-        end
-    end
-
-    # Reshape alpha into 100,000x1 vector with each of the 1,000 unique values repeated 100 times
-    alpha = reshape(repmat(alpha,1,100)', agents*bs, 1)
-
-    # Draw the income distribution:
-    Yit = zeros(bs*agents, T)
-    z = zeros(bs*agents, T)
-
-    for t = 1:T
-        for i = 1:bs*agents
-            Yit[i, t] = exp(alpha[i] + beta[i, t]*t + z[t] + sqrt(var_eps)*randn())
-            if t < T
-                z[i, t+1] = ar1rho*z[i, t] + sqrt(var_eta)*randn()
-            end
-        end
-    end
-    @printf "\tβ is between %.2f and %.2f, β_2 is between %.2f and %.2f\n" minimum(beta1)  maximum(beta1) minimum(beta2) maximum(beta2)
-
-end;
-
-# Calculate median income in last period for calculation of retirement benefits
-ymedian = median(Yit[:, 40])
-@printf "\tMedian income in period 40 is %.2f\n" ymedian
+  return yit
+end
