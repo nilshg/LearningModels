0a644f4c2baa9855380cd6a53191c63e26cbcce6
Nils <nilshg@hotmail.com>
2015-01-16 14:36:44 +0000

    Infer wpoints_R and ypoints_R

--- NHL/NHL_4_Retirement.jl
+++ NHL/NHL_4_Retirement.jl
@@ -2,18 +2,17 @@
 #########################    RETIREMENT PROBLEM      ##############################
 ###################################################################################
 
-function solveRetirement(wgrid_R::Array, ygrid_R::Array, wpoints_R::Int64,
-                         ypoints_R::Int64, r::Float64, δ::Float64)
+function solveRetirement(wgrid_R::Array, ygrid_R::Array, r::Float64, δ::Float64)
 
   @printf "4. Solving the retirement problem\n"
   tic()
-  tR = size(wgrid_R, 2)
-  v_R = Array(Float64, (wpoints_R, ypoints_R, tR))
+  tR = size(wgrid_R,2)
+  v_R = Array(Float64, (size(wgrid_R,1), size(ygrid_R,1), tR))
   wp_R = similar(v_R)
 
   # Value of last period of retirement
-  for w = 1:wpoints_R
-    for y = 1:ypoints_R
+  for w = 1:size(wgrid_R,1)
+    for y = 1:size(ygrid,1)
       wp_R[w, y, tR] = 0.0
       v_R[w, y, tR] = u(wgrid_R[w, tR] + ygrid_R[y])
     end
@@ -21,8 +20,8 @@ function solveRetirement(wgrid_R::Array, ygrid_R::Array, wpoints_R::Int64,
 
   # Compute the period tR-1 solution exactly:
   wmin = wgrid_R[1, tR]
-  for w = 1:wpoints_R
-    for y = 1:ypoints_R
+  for w = 1:size(wgrid_R,1)
+    for y = 1:size(ygrid,1)
       wt = wgrid_R[w, tR-1]
       yt = ygrid_R[y]
       xt = wt + yt
@@ -39,8 +38,8 @@ function solveRetirement(wgrid_R::Array, ygrid_R::Array, wpoints_R::Int64,
   for t = (tR-2):-1:1
     v_R_interpol = interpolatev(v_R, wgrid_R, ygrid_R, t+1)
     wmin = wgrid_R[1, t+1]
-    for w = 1:wpoints_R
-     for y = 1:ypoints_R
+    for w = 1:size(wgrid_R,1)
+     for y = 1:size(ygrid,1)
         (wp_R[w, y, t], v_R[w, y, t]) =
           bellOpt_R(wgrid_R[w, t], ygrid_R[y], wmin, v_R_interpol, r, δ)
       end
