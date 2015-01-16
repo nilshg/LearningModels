944c05843ac7c0c3f6220bac7ce1b330f4b5130a
Nils <nilshg@hotmail.com>
2015-01-16 11:37:00 +0000

    Change function calls

--- NHL/NHL_0_JuliaCode.jl
+++ NHL/NHL_0_JuliaCode.jl
@@ -30,12 +30,12 @@ end
                          var_η, var_ɛ, guvenen_distribution)
 # 3. Construct Grids
 (wgrid, agrid, bgrid, zgrid, wgrid_R, ygrid_R) =
-  grids(s_f_i, stdy, agents, bs, power, wpoints, apoints, bpoints, zpoints,
-        wpoints_R, ypoints_R, wmaxR, r, tW, tR, false, true)
+  grids(s_f_i, stdy, wpoints, apoints, bpoints, zpoints,
+        wpoints_R, ypoints_R, wmaxR, power, r, tR, false, true)
 
 # 4. Solve Retirement Problem
 (v_R, wp_R) =
-  solveRetirement(wgrid_R, ygrid_R, wpoints_R, ypoints_R, r, δ, tR)
+  solveRetirement(wgrid_R, ygrid_R, wpoints_R, ypoints_R, r, δ)
 
 # 5. Solve Transition Problem
 #(v, wp, constrained) =
@@ -43,13 +43,13 @@ end
 
 # 5.1 Solve Terminal Period Problem instead of 4. and 5.
 (wp, c, v, c_over_x) =
-  solveTransition(wgrid, agrid, bgrid, zgrid, r, δ, tW)
+  solveTransition(wgrid, agrid, bgrid, zgrid, r, δ)
 
 # 6. Solve Working Life Problem
 (v, wp, c_over_x) =
-  solveWorkingLife(v, wp, c_over_x, wgrid, agrid, bgrid, zgrid, stdy, r, δ, tW)
+  solveWorkingLife(v, wp, c_over_x, wgrid, agrid, bgrid, zgrid, stdy, r, δ)
 
 # 7. Simulate consumption and wealth distributions
 (c_t, w_t, wp_t) =
   simulate(wp, wgrid, agrid, bgrid,  zgrid, yit, ymedian, s_f_i, wp_R, wgrid_R,
-           ygrid_R, pension, r, agents, bs, tW, tR)
+           ygrid_R, pension, r)
