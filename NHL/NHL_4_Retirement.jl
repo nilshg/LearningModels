2e5e900bc0e72beb4f75fa8b5175850cb057913b
Nils <nilshg@hotmail.com>
2015-02-23 16:53:28 +0000

    Analytical solution to retirement
    Based on [Carroll's
    notes](http://www.econ2.jhu.edu/people/ccarroll/public/LectureNotes/Consumption/PerfForesightCRRA/)

--- NHL/NHL_4_Retirement.jl
+++ NHL/NHL_4_Retirement.jl
@@ -57,3 +57,57 @@ function solveRetirement(wgrid_R::Array, ygrid_R::Array, r::Float64, δ::Float64
 
   return v_R, wp_R, c_over_x
 end
+
+
+################################################################################
+
+function solveRetirement(wgrid_R::Array, ygrid_R::Array, r::Float64, δ::Float64,
+                         σ::Float64)
+
+  function get_c_1(r::Float64, δ::Float64, x::Float64, y::Float64,
+                   σ::Float64, tR::Int64)
+    Rinv = 1/r
+    Rbsig = (r*δ)^(1/σ)
+    numerator = 1 - Rinv*Rbsig
+    denominator = 1 - (Rinv*Rbsig)^tR
+    margprop = numerator/denominator
+    pdvlabour = y*((1-Rinv^tR)/(1-Rinv))
+    pdvresources = pdvlabour + x
+    c_1 = margprop * pdvresources
+
+    return c_1
+  end
+
+  function simulate(x::Float64, y::Float64, δ::Float64, σ::Float64,
+                    r::Float64, tR::Int64)
+    c_t = Array(Float64, tR)
+    x_t = similar(c_t)
+    sum_u = 0.0
+
+    c_t[1] = get_c_1(r, δ, x, y, σ, tR)[1]
+    x_t[1] = x + y
+
+    for t = 1:tR-1
+      c_t[t+1] = (r*δ)^(1/σ)*c_t[t]
+      x_t[t+1] = (x_t[t] - c_t[t])*r + y
+      sum_u += (δ^t)*(c_t[t]^(1-σ)/(1-σ))
+    end
+    sum_u += (δ^tR)*(c_t[tR]^(1-σ)/(1-σ))
+
+    return x_t, sum_u
+  end
+
+  wp_R = Array(Float64, (size(wgrid_R,1), size(ygrid_R,1), size(wgrid_R,2)))
+  v_R = Array(Float64, (size(wgrid_R,1), size(ygrid_R,1), 1))
+
+  for w = 1:size(wgrid_R,1)
+    for y = 1:size(ygrid_R,1)
+      xt = wgrid_R[w]
+      yt = ygrid_R[y]
+
+      (wp_R[w, y, :], v_R[w, y]) =
+        simulate(xt, yt, δ, σ, r, tR)
+    end
+  end
+  return wp_R, v_R
+end
