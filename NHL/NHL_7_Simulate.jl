b4662ea8c757a0df3462b426492995c27e537a09
Nils <nilshg@hotmail.com>
2015-02-27 12:08:39 +0000

    Simulate using ApproXD
    allows for usage of irregular grids

--- NHL/NHL_7_Simulate.jl
+++ NHL/NHL_7_Simulate.jl
@@ -20,14 +20,15 @@ function simulate(wp::Array, wgrid::Array, agrid::Array, bgrid::Array,
   negconscounter = 0
   for t = 1:tW
     # INTERPOLATION
-    wp_int = interpolatev(wp, wgrid, agrid, bgrid, zgrid, t)
+    wp_int =
+      interpolatev_A(wp[:, :, :, :, t], wgrid[:, t], agrid, bgrid, zgrid)
     # Bond Choice
     for i = 1:size(yit,1)
       wt = w_t[i, t]
       yt = yit[i, t]
       (at, bt, zt) = s_f_i[:, i, t]
       xt = wt + yt
-      wp_t[i, t] = wp_int[xt, at, bt, zt]
+      wp_t[i, t] = getValue(wp_int, [xt, at, bt, zt])[1]
       c_t[i, t] = xt - wp_t[i, t]
       w_t[i, t+1] = r*wp_t[i, t]
 
@@ -39,23 +40,40 @@ function simulate(wp::Array, wgrid::Array, agrid::Array, bgrid::Array,
   negconscounter == 0 || @printf "\t%d negative consumption choices!\n" negconscounter
 
   @printf "\tSimulating %d periods of retirement...\n" tR
-  negconscounter = 0
-  for t = (tW+1):(tW+tR)
-    wp_int = interpolatev(wp_R, wgrid_R, ygrid_R, t-tW)
+  function get_c_1(r::Float64, δ::Float64, x::Float64, y::Float64,
+                   σ::Float64, tR::Int64)
+    Rinv = 1/r
+    Rbsig = (r*δ)^(1/σ)
+    numerator = 1 - Rinv*Rbsig
+    denominator = 1 - (Rinv*Rbsig)^tR
+    margprop = numerator/denominator
+    pdvlabour = y*((1-Rinv^tR)/(1-Rinv))
+    pdvresources = pdvlabour + x
+    c_1 = margprop * pdvresources
 
-    for  i = 1:size(yit,1)
-      wt = w_t[i, t]
-      yt = pension[i]
-      xt = wt + yt
-
-      wp_t[i, t] = wp_int[xt, yt]
-      c_t[i, t] = xt - wp_t[i, t]
-
-      if  t < tW + tR
-        w_t[i, t+1] = r*wp_t[i, t]
-      end
-    end
+    return c_1
   end
-  negconscounter == 0 || @printf "\t%d negative consumption choices!\n" negconscounter
+
+  function simulate_i(x::Float64, y::Float64, δ::Float64, σ::Float64,
+                    r::Float64, tR::Int64)
+    c_R_i = Array(Float64, tR)
+    x_t = similar(c_R_i)
+    c_R_i[1] = get_c_1(r, δ, x, y, σ, tR)[1]
+    x_t[1] = x + y
+
+    for t = 1:tR-1
+      c_R_i[t+1] = (r*δ)^(1/σ)*c_R_i[t]
+      x_t[t+1] = (x_t[t] - c_R_i[t])*r + y
+    end
+    return c_R_i, x_t
+  end
+
+  for i = 1:size(yit,1)
+    (a,b) =
+      simulate_i(w_t[i, tW+1], pension[i], δ, σ, r, tR)
+    c_t[i, 41:70] = a
+    w_t[i, 41:70] = b
+  end
+
   return c_t, w_t, wp_t
 end
