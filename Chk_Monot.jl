9d89f27f1ecd2047b837a3906582e7f6907121a5
Nils <nilshg@hotmail.com>
2015-08-11 17:58:30 +0100

    Update Chk_Monot
    Mainly stylistic differences, important to note that habits are sorted
    the other way around, so we need `rev=true` kwarg

--- Chk_Monot.jl
+++ Chk_Monot.jl
@@ -1,106 +1,84 @@
 # Check monotonicity
 
-function checkmonotonicity(v::Array{Float64, 6}, t::Int64)
-
-  vnow = v[:, :, :, :, :, t]
-  vsize = size(vnow)
-  dim1count = 0
-  dim2count = 0
-  dim3count = 0
-  dim4count = 0
-  dim5count = 0
+function checkmonotonicity(v::Array{Float64,5})
+  counter = zeros(5)
   # Monotonicity in first dimension
-  for f = 1:vsize[5], d = 1:vsize[4], c = 1:vsize[3], b = 1:vsize[2]
-    dim1count += ~issorted(vnow[:, b, c, d, f])
+  for f = 1:size(v,5), d = 1:size(v,4), c = 1:size(v,3), b = 1:size(v,2)
+    counter[1] += ~issorted(v[:, b, c, d, f])
   end
   # Monotonicity in second dimension
-  for f = 1:vsize[5], d = 1:vsize[4], c = 1:vsize[3], a = 1:vsize[1]
-    dim2count += issorted(vnow[a, :, c, d, f])
+  for f = 1:size(v,5), d = 1:size(v,4), c = 1:size(v,3), a = 1:size(v,1)
+    counter[2] += ~issorted(v[a, :, c, d, f], rev=true)
   end
   # Monotonicity in third dimension
-  for f = 1:vsize[5], d = 1:vsize[4], b = 1:vsize[2], a = 1:vsize[1]
-    dim3count += ~issorted(vnow[a, b, :, d, f])
+  for f = 1:size(v,5), d = 1:size(v,4), b = 1:size(v,2), a = 1:size(v,1)
+    counter[3] += ~issorted(v[a, b, :, d, f])
   end
   # Monotonicity in fourth dimension
-  for f = 1:vsize[5], c = 1:vsize[3], b = 1:vsize[2], a = 1:vsize[1]
-    dim4count += ~issorted(vnow[a, b, c, :, f])
+  for f = 1:size(v,5), c = 1:size(v,3), b = 1:size(v,2), a = 1:size(v,1)
+    counter[4] += ~issorted(v[a, b, c, :, f])
   end
   # Monotonicity in fifth dimension
-  for d = 1:vsize[4], c = 1:vsize[3], b = 1:vsize[2], a = 1:vsize[1]
-    dim5count += ~issorted(vnow[a, b, c, d, :])
+  for d = 1:size(v,4), c = 1:size(v,3), b = 1:size(v,2), a = 1:size(v,1)
+    counter[5] += ~issorted(v[a, b, c, d, :])
   end
 
-  return dim1count, dim2count, dim3count, dim4count, dim5count
+  return counter
 end
 
 
-function checkmonotonicity(v::Array{Float64, 5}, t::Int64)
-
-  vnow = v[:, :, :, :, t]
-  vsize = size(vnow)
-  dim1count = 0
-  dim2count = 0
-  dim3count = 0
-  dim4count = 0
+function checkmonotonicity(v::Array{Float64,4})
+  counter = zeros(4)
   # Monotonicity in first dimension
-  for d = 1:vsize[4], c = 1:vsize[3], b = 1:vsize[2]
-    dim1count += ~issorted(vnow[:, b, c, d])
+  for d = 1:size(v,4), c = 1:size(v,3), b = 1:size(v,2)
+    counter[1] += ~issorted(v[:, b, c, d])
   end
   # Monotonicity in second dimension
-  for d = 1:vsize[4], c = 1:vsize[3], a = 1:vsize[1]
-    dim2count += ~issorted(vnow[a, :, c, d])
+  for d = 1:size(v,4), c = 1:size(v,3), a = 1:size(v,1)
+    counter[2] += ~issorted(v[a, :, c, d])
   end
   # Monotonicity in third dimension
-  for d = 1:vsize[4], b = 1:vsize[2], a = 1:vsize[1]
-    dim3count += ~issorted(vnow[a, b, :, d])
+  for d = 1:size(v,4), b = 1:size(v,2), a = 1:size(v,1)
+    counter[3] += ~issorted(v[a, b, :, d])
   end
   # Monotonicity in fourth dimension
-  for c = 1:vsize[3], b = 1:vsize[2], a = 1:vsize[1]
-    dim4count += ~issorted(vnow[a, b, c, :])
+  for c = 1:size(v,3), b = 1:size(v,2), a = 1:size(v,1)
+    counter[4] += ~issorted(v[a, b, c, :])
   end
 
-  return dim1count, dim2count, dim3count, dim4count
+  return counter
 end
 
 
-function checkmonotonicity(v::Array{Float64, 4}, t::Int64)
-
-  vnow = v[:, :, :, t]
-  vsize = size(vnow)
-  dim1count = 0
-  dim2count = 0
-  dim3count = 0
+function checkmonotonicity(v::Array{Float64,3})
+  counter = zeros(3)
   # Monotonicity in first dimension
-  for c = 1:vsize[3], b = 1:vsize[2]
-    dim1count += ~issorted(vnow[:, b, c])
+  for c = 1:size(v,3), b = 1:size(v,2)
+    counter[1] += ~issorted(v[:, b, c])
   end
   # Monotonicity in second dimension
-  for c = 1:vsize[3], a = 1:vsize[1]
-    dim2count += ~issorted(-vnow[a, :, c])
+  for c = 1:size(v,3), a = 1:size(v,1)
+    counter[2] += ~issorted(v[a, :, c], rev=true)
   end
   # Monotonicity in third dimension
-  for b = 1:vsize[2], a = 1:vsize[1]
-    dim3count += ~issorted(vnow[a, b, :])
+  for b = 1:size(v,2), a = 1:size(v,1)
+    counter[3] += ~issorted(v[a, b, :])
   end
 
-  return dim1count, dim2count, dim3count
+  return counter
 end
 
 
-function checkmonotonicity(v::Array{Float64, 3}, t::Int64)
-
-  vsize = size(v)
-  dim1count = 0
-  dim2count = 0
+function checkmonotonicity(v::Array{Float64,2})
+  counter = zeros(2)
   # Monotonicity in first dimension
-  for b = 1:vsize[2]
-    dim1count += ~issorted(v[:, b, t])
+  for b = 1:size(v,2)
+    counter[1] += ~issorted(v[:, b])
   end
-
   # Monotonicity in second dimension
-  for a = 1:vsize[1]
-    dim2count += ~issorted(v[a, :, t])
+  for a = 1:size(v,1)
+    counter[2] += ~issorted(v[a, :])
   end
 
-  return dim1count, dim2count
+  return counter
 end
