64ea249049d47bed032e8b1d6d96da7142dbffc7
Nils <nilshg@hotmail.com>
2015-08-12 11:58:33 +0100

    Re-add Grid dependency to Interpolations

--- Interpolations.jl
+++ Interpolations.jl
@@ -1,41 +1,8 @@
 #######################################################################################
-################## Interpolations - Interpolation for value Functions #################
+#################### Interpolations for value and policy functions ####################
 #######################################################################################
 
-# Contains:
-# interpolateV(V::Array{Float64, 5}, wgrid, ygrid, agrid, bgrid, zgrid, t):
-#     Interpolate value function w/o habits using CoordInterpGrid
-# interpolateV(V::Array{Float64, 3}, wgrid, hgrid, ygrid, t):
-#     Interpolate retirement value w/o habits function using CoordInterpGrid function
-
-#######################################################################################
-
-using ApproXD
-
-#######################################################################################
-
-function interpolateV(v::Array{Float64,4}, xgrid::Array{Float64,1},
-                      agrid::Array{Float64,1}, bgrid::Array{Float64,1},
-                      zgrid::Array{Float64,1})
-
-  xabz = Array{Float64,1}[]
-  push!(xabz, xgrid)
-  push!(xabz, agrid)
-  push!(xabz, bgrid)
-  push!(xabz, zgrid)
-  return Lininterp(v, xabz)
-end
-
-#######################################################################################
-
-function interpolateV(v::Array{Float64,2}, xgrid::Array{Float64,1},
-                      ygrid::Array{Float64,1})
-
-  xy = Array{Float64,1}[]
-  push!(xy, xgrid)
-  push!(xy, ygrid)
-  return Lininterp(v, xy)
-end
+using ApproXD, Grid
 
 #######################################################################################
 
@@ -54,14 +21,30 @@ end
 
 #######################################################################################
 
+function interpolateV(v::Array{Float64,4}, xgrid::Array{Float64,1},
+            agrid::Array{Float64,1}, bgrid::Array{Float64,1}, zgrid::Array{Float64,1})
+
+  xabz = Array{Float64,1}[]
+  push!(xabz, xgrid); push!(xabz, agrid); push!(xabz, bgrid); push!(xabz, zgrid)
+  return Lininterp(v, xabz)
+end
+
+#######################################################################################
+
 function interpolateV(v::Array{Float64,3}, xgrid::Array{Float64,1},
                       ygrid::Array{Float64,1}, hgrid::Array{Float64,1})
 
   xyh = Array{Float64,1}[]
-  push!(xyh, xgrid)
-  push!(xyh, ygrid)
-  push!(xyh, hgrid)
+  push!(xyh, xgrid); push!(xyh, ygrid); push!(xyh, hgrid)
   return Lininterp(v, xyh)
 end
 
 #######################################################################################
+
+function interpolateV(v::Array{Float64,2}, xgrid::Array{Float64,1},
+                      ygrid::Array{Float64,1})
+
+  xy = Array{Float64,1}[]
+  push!(xy, xgrid); push!(xy, ygrid)
+  return Lininterp(v, xy)
+end
