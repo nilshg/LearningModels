e650da4dd41665d8484834d02d30e8198404ffa5
Nils <nilshg@hotmail.com>
2015-08-28 11:14:15 +0100

    Correct parallel call
    As per Steven G Johnson's recommendation, `import` first, then do
    `using` in auxiliary files.

--- NHL/NHL_0_JuliaCode.jl
+++ NHL/NHL_0_JuliaCode.jl
@@ -1,45 +1,41 @@
-path=("C:/Users/tew207/My Documents/GitHub/LearningModels/")
-include(path*"Optimizations.jl")
-include(path*"Interpolations.jl")
-include(path*"Parameters.jl")
-include(path*"NHL/NHL_6_Bellman.jl")
-include(path*"Chk_Monot.jl")
-include(path*"1_Income.jl")
-include(path*"2_Learning.jl")
-include(path*"NHL/NHL_3_Grid.jl")
-include(path*"NHL/NHL_4_Retirement.jl")
-include(path*"NHL/NHL_5_Transition.jl")
-include(path*"NHL/NHL_7_Simulate.jl")
-include(path*"NHL/NHL_Diagnostics.jl")
+nprocs()==CPU_CORES || addprocs(CPU_CORES-1)
+import ApproXD, Grid, Distributions, Optim, QuantEcon, PyPlot, PyCall
+@everywhere begin
+  path=("C:/Users/tew207/My Documents/GitHub/LearningModels/")
+  include(path*"Optimizations.jl")
+  include(path*"Interpolations.jl")
+  include(path*"Parameters.jl")
+  include(path*"1_Income.jl")
+  include(path*"2_Learning.jl")
+  include(path*"NHL/NHL_3_Grid.jl")
+  include(path*"NHL/NHL_4_Retirement.jl")
+  include(path*"NHL/NHL_5_Transition.jl")
+  include(path*"NHL/NHL_6_Bellman.jl")
+  include(path*"NHL/NHL_7_Simulate.jl")
+end
 
 # 1. Draw Income Distribution
-(yit, ymedian, pension) = incomeDistribution(
-  "C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code/LaborReal.dat",
-  "C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code/alfabeta.dat")
+(yit, ymedian, pension) = incomeDistribution("tew207")
 
 # 2. Construct individual specific belief histories
-(s_f_i, stdy, k) = learning(
-    "C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code")
+(s_f_i, stdy, k) = learning("tew207")
 
 # 3. Construct Grids
-(wgrid, agrid, bgrid, zgrid, wgrid_R, ygrid_R) =
-  grids(s_f_i, apoints, bpoints, zpoints, ypoints_R, r, true,
-    "C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN code/wealth.dat",
-    "C:/Users/tew207/Dropbox/QMUL/PhD/Code/Guvenen FORTRAN Code/wealthR.dat")
+(xgrid, agrid, bgrid, zgrid, wgrid_R, ygrid_R) =
+  grids(s_f_i, apoints, bpoints, zpoints, ypoints_R, r, true, "tew207")
 
 # 4. Solve Retirement Problem
-(v_R, wp_R) = solveRetirement(wgrid_R, convert(Array,ygrid_R), r, δ, σ)
+(v_R, wp_R) = solveRetirement(wgrid_R, ygrid_R, r, δ, σ)
 
 # 5. Solve Terminal Period Problem instead of 4. and 5.
-(wp, v, c_over_x) =
-  solveTransition(v_R, wgrid_R, ygrid_R, wgrid, agrid, bgrid,
-  zgrid, yit, r, δ)
+(v, wp, c_over_x) =
+  solveTransition(v_R, wgrid_R, ygrid_R, xgrid, agrid, bgrid, zgrid, yit, r, δ)
 
 # 6. Solve working life problem
-(v, wp, c_over_x) =
-  solveWorkingLife(v, wp, c_over_x, wgrid, agrid, bgrid, zgrid, stdy, k, r, δ)
+@time (v, wp, c_over_x) =
+  solveWorkingLife(v, wp, xgrid, agrid, bgrid, zgrid, stdy, k, r, δ, c_over_x)
 
 # 7. Simulate wealth distribution
 (c_t, w_t, wp_t) =
-  simulate(wp, wgrid, agrid, bgrid,  zgrid, yit, ymedian, s_f_i, wp_R, wgrid_R,
+  sim(wp, xgrid, agrid, bgrid,  zgrid, yit, ymedian, s_f_i, wp_R, wgrid_R,
            ygrid_R, pension, r)
