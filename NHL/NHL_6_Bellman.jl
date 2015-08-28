e76bfdf6abc09199178fe4add180e3f629fcf133
Nils <nilshg@hotmail.com>
2015-08-28 14:58:05 +0100

    Fix typo

--- NHL/NHL_6_Bellman.jl
+++ NHL/NHL_6_Bellman.jl
@@ -18,15 +18,15 @@ function solveWorkingLife{T<:AbstractFloat}(v::Array{T,5}, wp::Array{T,5},
       interpolateV(v[:, :, :, :, t+1], xgrid[:, t+1], agrid, bgrid, zgrid)
 
     # MAXIMIZATION
-    xmin = xgrid[1, t+1]
+    wmin = xgrid[1, t+1]
     @sync @parallel for x = 1:size(xgrid,1)
       for a = 1:size(agrid,1), b = 1:size(bgrid,1), z = 1:size(zgrid,1)
         at = agrid[a]; bt = bgrid[b]; zt = zgrid[z]; xt = xgrid[x, t]
         yln = LogNormal(at + bt*(t+1) + ρ*zt, stdy[t])
 
-        (xt - xmin/r < 0.01) ? xt = xmin/r + 0.01 : 0
+        (xt - xmin/r < 0.01) ? xt = wmin/r + 0.01 : 0
         (wpnow[x, a, b, z], vnow[x, a, b, z]) =
-          bellOpt(xt, at, bt, zt, xmin, v_interpol, yln, k[:, t], ρ, r, δ)
+          bellOpt(xt, at, bt, zt, wmin, v_interpol, yln, k[:, t], ρ, r, δ)
 
         cxnow[x,a,b,z] = (xt-wpnow[x,a,b,z])/xt
       end
