02b409c239948e98e26a6505f4724617cb632b2c
Nils <nilshg@hotmail.com>
2015-09-11 14:39:14 +0100

    Add trilinear interpolation for each w

--- Interpolations.jl
+++ Interpolations.jl
@@ -47,3 +47,15 @@ function interpolateV(v::Array{Float64,2}, xgrid::Array{Float64,1},
   push!(xy, xgrid); push!(xy, ygrid)
   return Lininterp(v, xy)
 end
+
+################################################################################
+
+function get_lins{T<:AbstractFloat}(v::Array{T,4}, xgrid::Array{T,1},
+  agrid::Array{T,1}, bgrid::Array{T,1}, zgrid::Array{T,1}, t::Int64)
+
+  lin_int = Array(Lininterp, length(xgrid))
+  for x = 1:length(xgrid)
+    lin_int[x] = interpolateV(squeeze(v[x,:,:,:], 1), agrid, bgrid, zgrid)
+  end
+  return lin_int
+end
