96f3f292176a2397db19f8b58e08e6b83ba597cc
Nils <nilshg@hotmail.com>
2015-09-13 19:03:49 +0100

    Streamline print output

--- NHL/NHL_6_Bellman.jl
+++ NHL/NHL_6_Bellman.jl
@@ -6,22 +6,18 @@ function solveWorkingLife{T<:AbstractFloat}(v::Array{T,5}, wp::Array{T,5},
   xgrid::Array{T,2}, agrid::Array{T,1}, bgrid::Array{T,1}, zgrid::Array{T,1},
   stdy::Array{T,1}, k::Array{T,2}, r::T, δ::T, ρ::T, c_over_x::Array{T,5})
 
-  @printf "6. Recursively solve for optimal decision rules\n"
-  @printf "\tSolving on %d points..." length(v[:, :, :, :, end])
-
+  println("Solve for policy function on $(prod(size(v)[1:4])) points")
   wpnow = SharedArray(Float64, size(v)[1:4], pids=procs()); vnow=similar(wpnow)
   cxnow = similar(wpnow)
 
   for t = (size(xgrid,2)-1):-1:1
     # INTERPOLATION
-    v_interpol =
-      interpolateV(v[:, :, :, :, t+1], xgrid[:, t+1], agrid, bgrid, zgrid)
+    v_interpol = interpolateV(v[:,:,:,:,t+1], xgrid[:,t+1], agrid, bgrid, zgrid)
 
     # MAXIMIZATION
     wmin = xgrid[1, t+1]
     @inbounds @sync @parallel for x = 1:size(xgrid,1)
       for a = 1:size(agrid,1), b = 1:size(bgrid,1), z = 1:size(zgrid,1)
-        #x=3;a=2;b=5;z=4;
         at = agrid[a]; bt = bgrid[b]; zt = zgrid[z]; xt = xgrid[x, t]
         yn = Normal(at + bt*(t+1) + ρ*zt, stdy[t])
 
