3dd37e545b5c9a00ea8d5e2ff1cf17fff19166a4
Nils <nilshg@hotmail.com>
2016-04-03 15:07:33 +0100

    Add bequest parameter to Jacobian calculation

--- NHL/NHL_Jacobian.jl
+++ NHL/NHL_Jacobian.jl
@@ -1,44 +1,46 @@
 using StatsBase
 
-function jacobian{T<:AbstractFloat}(dopt::T, sopt::T, wgrid_R=wgrid_R, ygrid_R=ygrid_R,
+function jacobian{T<:AbstractFloat}(dopt::T, sopt::T, uopt::T, wgrid_R=wgrid_R, ygrid_R=ygrid_R,
   r=r, tR=tR, xgrid=xgrid, agrid=agrid, bgrid=bgrid, zgrid=zgrid, yit=yit, g_t=g_t,
   stdy=stdy, k=k, ρ=ρ, s_f_i=s_f_i, pension=pension)
 
-  v_R, wp_R = solveRetirement(wgrid_R, ygrid_R, r, δ, σ, tR)
+  v_R, wp_R = solveRetirement(wgrid_R, ygrid_R, r, δ, σ, tR, υ)
   v, wp, c_over_x = solveTransition(v_R, wgrid_R, ygrid_R, xgrid, agrid,
-                                      bgrid, zgrid, yit, g_t, r, δ)
+                                      bgrid, zgrid, yit, g_t, r, δ, σ)
   v, wp, c_over_x = solveWorkingLife(v, wp, xgrid, agrid, bgrid, zgrid,
-                                              stdy, k, r, δ, ρ, c_over_x, g_t)
-  c_t, w_t, wp_t, pct = sim(wp, xgrid, agrid, bgrid,  zgrid, wgrid_R, yit, s_f_i,
-                                                          pension, r, δ, σ, tR)
+                                    stdy, k, r, δ, ρ, c_over_x, g_t, σ, 0.0)
+  c_t, w_t, wp_t, pct = sim(wp, wp_R, xgrid, agrid, bgrid, zgrid, wgrid_R,
+                                  ygrid_R, yit, s_f_i, pension, r, δ, σ, tR)
 
   mopt = collect(pct[10:90,:])
-  perturb = [dopt*(1.005) sopt;
-             dopt/(1.005) sopt;
-             dopt sopt*(1.005);
-             dopt sopt/(1.005)]
-  pertresults = zeros(length(mopt),4)
+  perturb = [dopt+0.005 sopt uopt;
+             dopt-0.005 sopt uopt;
+             dopt sopt+0.05 uopt;
+             dopt sopt-0.05 uopt;
+             dopt sopt uopt+0.05;
+             dopt sopt uopt-0.05]
+  pertresults = zeros(length(mopt),6)
 
-  for i = 4:4
-    δ = perturb[i,1]; σ = perturb[i,2]
-    v_R, wp_R = solveRetirement(wgrid_R, ygrid_R, r, δ, σ, tR)
+  for i = 1:6
+    δ = perturb[i,1]; σ = perturb[i,2]; υ = perturb[i,3]
+    v_R, wp_R = solveRetirement(wgrid_R, ygrid_R, r, δ, σ, tR, υ)
     v, wp, c_over_x = solveTransition(v_R, wgrid_R, ygrid_R, xgrid, agrid,
-                                        bgrid, zgrid, yit, g_t, r, δ)
+                                        bgrid, zgrid, yit, g_t, r, δ, σ)
     v, wp, c_over_x = solveWorkingLife(v, wp, xgrid, agrid, bgrid, zgrid,
-                                                stdy, k, r, δ, ρ, c_over_x, g_t)
-    c_t, w_t, wp_t, pct = sim(wp, xgrid, agrid, bgrid,  zgrid, wgrid_R, yit, s_f_i,
-                                                            pension, r, δ, σ, tR)
+                                      stdy, k, r, δ, ρ, c_over_x, g_t, σ, 0.0)
+    c_t, w_t, wp_t, pct = sim(wp, wp_R, xgrid, agrid, bgrid, zgrid, wgrid_R,
+                                    ygrid_R, yit, s_f_i, pension, r, δ, σ, tR)
 
     pertresults[:,i] = collect(pct[10:90,:])
   end
 
-  J = zeros(length(mopt),2)
+  J = zeros(length(mopt),3)
   J[:,1] = 0.5*(pertresults[:,1] - mopt)/0.005
          + 0.5*(pertresults[:,2] - mopt)/(-0.005)
   J[:,2] = 0.5*(pertresults[:,3] - mopt)/0.05
          + 0.5*(pertresults[:,4] - mopt)/(-0.05)
+  J[:,3] = 0.5*(pertresults[:,5] - mopt)/0.05
+         + 0.5*(pertresults[:,6] - mopt)/(-0.05)
 
   stderr = sqrt(diag(inv(J'*J)))
 end
-
-stderr = jacobian(0.99, 1.54)
